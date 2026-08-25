b4a05c5b2654efdaffbec16801752dab84cba920
moroxixi <mohamadrofialwani321@gmail.com>
2026-08-25 21:09:25 +0700

    auto: 2026-08-25 21:09

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 var MORODUIT_CONFIG = {
-  APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbw1KAc20Yvhv7byckEeuy5ahs5AtItrZYDhZ5kg8EVJuw9L3YL5F9rBAA7ui9liWUMf/exec",
+  APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbz0xQZXqksiIRvNUM-J34gPjFsvnIWzPgtEzdZjYjBI_Cybafcxz9z2eZaEyjeRkHKM/exec",
   TOKEN: "c47f9fb4af7826203f70ff7812976ce7f0eb83f04097961a",
   NOMOR_WA_TOKO: "6283156569954"
 };
